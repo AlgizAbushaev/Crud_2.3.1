d5977292d8c99a2a682f8d776cd7ba3a8c214951
Algiz Abushaev <103070605+marta-gavricheva@users.noreply.github.com>
2022-12-10 17:05:14 +0300

    Практическая задача 2.2.2

--- PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -1,16 +1,11 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarService;
 import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
